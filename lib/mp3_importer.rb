2d359dd2c184b48dc88fd3c027e49448601c16b1
Emily Wessel <ettl02@gmail.com>
2018-07-04 21:08:54 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -13,4 +13,6 @@ class MP3Importer
     self.files.each do |file|
       song = song.new_by_filename (file)
       Artist.all << song.artist unless Artist.all.include? (song.artist)
- 
+    end
+  end
+end
