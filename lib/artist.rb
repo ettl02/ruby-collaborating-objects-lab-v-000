0f3e29e3f4d1684f22d67c6273cd38f36203f32c
Emily Wessel <ettl02@gmail.com>
2018-07-04 21:02:26 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,4 +17,9 @@ class Artist
   end 
 
   def self.find_or_create_by_name (name)
+    self.all.detect { |artist| artist.name == name } || Artist.new (name)
+  end 
+
+  def print_songs 
+    @songs.each { |song| puts.song.name }
     
