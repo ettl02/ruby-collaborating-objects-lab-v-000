bb0af1d591a150501d353356dfd93141cbaa917b
Emily Wessel <ettl02@gmail.com>
2018-07-04 21:08:07 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,4 +7,9 @@ class MP3Importer
   def files
     @files = Dir.entries (@path)
     @files.delete_if {|file| file == "." || file == ".."}
-end
+  end
+
+  def import
+    self.files.each do |file|
+      song = song.new_by_filename (file)
+      Artist
